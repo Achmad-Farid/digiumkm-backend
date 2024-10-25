0d88c5050da1f8b8d8a900a1aa2dcd8d6cd2b9cc
farid <valkanid12@gmail.com>
2024-10-25 23:10:07 +0700

    sen

--- models/webinarModel.js
+++ models/webinarModel.js
@@ -4,7 +4,6 @@ const commentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String }, // Tambahkan properti untuk menyimpan nama pengguna
   comment: { type: String, required: true },
-  sentiment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
